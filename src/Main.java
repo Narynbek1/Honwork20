4fed099d166b7fc5fd57b1fb7de86d37d550bcfd
naryn <narynbeknurbekov@gmail.com>
2023-05-03 21:10:43 +0300

    c

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <=10 ; i++) {
-            for (int j = 1; j <=10; j++) {
-                System.out.printf("\n"+i+"x"+j+"="+(i*j)+"\t");
-            }
-            System.out.println();
+//        for (int i = 1; i <=10 ; i++) {
+//            for (int j = 1; j <=10; j++) {
+//                System.out.printf("\n"+i+"x"+j+"="+(i*j)+"\t");
+//            }
+//            System.out.println();
+//        }
+//
+
+
+        try (Car car = new Car()) {
+            car.drive();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
